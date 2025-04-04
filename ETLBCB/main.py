3d069d8b675429f6ad75fabf7e2dcc1341cc70ed
Rihan Cabral <163031225+RihanCabral@users.noreply.github.com>
2025-04-04 16:15:00 -0300

    Delete ETLBCB directory

--- ETLBCB/main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from src.extractTransform import requestApiBcb
-from src.load import salvarCsv
-
-dadosBcb = requestApiBcb('20191')
-salvarCsv(dadosBcb, "src/datasetsmeiosPagamentosTri.csv", ";", ".")
